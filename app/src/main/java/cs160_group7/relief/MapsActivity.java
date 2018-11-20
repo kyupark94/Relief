2118d050e9ca6e3f2e1020f9346ceb025980824e
xdddmw <xiongqw@berkeley.edu>
2018-11-19 23:21:40 -0800

    adjusted maps

--- app/src/main/java/cs160_group7/relief/MapsActivity.java
+++ app/src/main/java/cs160_group7/relief/MapsActivity.java
@@ -63,10 +63,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(38, 122);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Campanile and move the camera
+        LatLng Campanile = new LatLng(37.8, -122.3);
+        mMap.addMarker(new MarkerOptions().position(Campanile).title("Marker in Campanile"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Campanile));
     }
 
     void initializeAdapter() {
